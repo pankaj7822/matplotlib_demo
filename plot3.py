d635c22fec57572eadf96b105dc79f4a644a764c
pankaj <pankaj.krjha.cse17@itbhu.ac.in>
2022-07-13 23:29:38 +0530

    plot 3 and 4

---
+++ plot3.py
@@ -0,0 +1,17 @@
+# importing the required libraries
+import matplotlib.pyplot as plt
+import numpy as np
+
+# define data values
+y1 = np.array([20210.60,21008.50,21586.50,20290.40,20019.80,19894.70,19777.80,19433.80,19110.40,19086.20,19036.10,19022.50,19019.60]) # X-axis points
+x1 = np.array([i+1 for i in range(len(y1))]) 
+
+y2=np.array([20010.60,20860.90,20568.60,20280.50,20008.80,19666.80,19589.60,19285.60,19022.60,18922.70,18888.80,18813.40,18800.80,18755.80,18742.60])
+x2 = np.array([i+1 for i in range(len(y2))])
+
+plt.plot(x1, y1,label="Price GA (₹)") # Plot the chart
+plt.plot(x2, y2,label="Price PGA (₹)") # Plot the chart
+plt.legend()
+plt.xlabel("Trials")
+plt.ylabel("Price")
+plt.savefig("plot3.jpeg")
---
+++ plot4.py
@@ -0,0 +1,17 @@
+# importing the required libraries
+import matplotlib.pyplot as plt
+import numpy as np
+
+# define data values
+y1 = np.array([34882.50,35267.60,35788.80,34290.60,34186.70,34040.00,33916.20,33890.40,33850.60,33844.00,33820.00,]) # X-axis points
+x1 = np.array([i+1 for i in range(len(y1))]) 
+
+y2=np.array([34000.00,34816.60,34980.60,34530.00,34375.20,34010.40,33910.00,33833.80,33696.10,33602.20,33540.60,33506.80,33426.40,33376.60,33290.80])
+x2 = np.array([i+1 for i in range(len(y2))])
+
+plt.plot(x1, y1,label="Price GA (₹)") # Plot the chart
+plt.plot(x2, y2,label="Price PGA (₹)") # Plot the chart
+plt.legend()
+plt.xlabel("Trials")
+plt.ylabel("Price")
+plt.savefig("plot4.jpeg")
